342454c5dd4b1871000d9999ea24779932fd31b1
Misael Guerrero <mguerrerowest@gmail.com>
2023-04-22 22:50:05 -0400

    Add test case examples for recursive solution

--- fibonacci/fibs.js
+++ fibonacci/fibs.js
@@ -12,3 +12,17 @@ function fibs(num) {
   }
   return answer
 }
+
+/*
+console.log(fibs(0)) -> []
+console.log(fibs(1)) -> [ 0 ]
+console.log(fibs(2)) -> [ 0, 1 ]
+console.log(fibs(3)) -> [ 0, 1, 1 ]
+console.log(fibs(4)) -> [ 0, 1, 1, 2 ]
+console.log(fibs(5)) -> [ 0, 1, 1, 2, 3 ]
+console.log(fibs(12)) -> [ 0,  1,  1,  2,  3, 5,  8, 13, 21, 34, 55, 89 ]
+console.log(fibs(20)) -> [ 0,   1,    1,    2,    3,
+                           5,   8,   13,   21,   34,
+                          55,  89,  144,  233,  377,
+                         610, 987, 1597, 2584, 4181 ]
+*/
--- fibonacci/fibsRec.js
+++ fibonacci/fibsRec.js
@@ -6,3 +6,17 @@ const fibsRec = function(num, answer = [], prev = 0, current = 1){
     answer.push(prev)
     return fibsRec(num, answer, current, next);
   }
+
+  /*
+console.log(fibsRec(0)) -> []
+console.log(fibsRec(1)) -> [ 0 ]
+console.log(fibsRec(2)) -> [ 0, 1 ]
+console.log(fibsRec(3)) -> [ 0, 1, 1 ]
+console.log(fibsRec(4)) -> [ 0, 1, 1, 2 ]
+console.log(fibsRec(5)) -> [ 0, 1, 1, 2, 3 ]
+console.log(fibsRec(12)) -> [ 0,  1,  1,  2,  3, 5,  8, 13, 21, 34, 55, 89 ]
+console.log(fibsRec(20)) -> [ 0,   1,    1,    2,    3,
+                              5,   8,   13,   21,   34,
+                             55,  89,  144,  233,  377,
+                            610, 987, 1597, 2584, 4181 ]
+*/
