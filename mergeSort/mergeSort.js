6bc7bb994e56945e16f0430d234506a39482408c
Misael Guerrero <mguerrerowest@gmail.com>
2023-04-24 09:49:27 -0400

    Add merge sort test case examples

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -40,3 +40,12 @@ function mergeSort(arr){
     
     return merge(leftSorted, rightSorted)
   }
+
+/*
+console.log(mergeSort([3,1,2])) -> [1, 2, 3]
+console.log(mergeSort([4,5,1,3,2])) -> [1, 2, 3, 4, 5]
+console.log(mergeSort([4,7,9,3,2,8,1,5,6])) -> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(mergeSort([200,34,98,521,123,67,908,420,12,3256,0])) -> [ 0,  12,   34,  67,
+                                                                     98, 123,  200, 420,
+                                                                    521, 908, 3256 ]
+*/
